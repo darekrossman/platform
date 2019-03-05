a786cd268dd9ae1a03aaa018593fdcabab175352
Darek Rossman <drossman@64labs.com>
2019-03-05 07:41:45 -0500

    bump

--- packages/ess/src/index.js
+++ packages/ess/src/index.js
@@ -1,27 +1,29 @@
-import React, { useEffect, useMemo, useContext, useState } from 'react'
-import { ThemeContext, jsx } from '@emotion/core'
-import deepEqual from 'deep-equal'
-import tokenizer from './tokenizer'
-import { getVariantESS } from './utils'
+import React, { useEffect, useMemo, useContext, useState } from "react"
+import { ThemeContext, jsx } from "@emotion/core"
+import deepEqual from "deep-equal"
+import tokenizer from "./tokenizer"
+import { getVariantESS } from "./utils"
 
 const useTheme = () => useContext(ThemeContext)
 
-export default (
-  label,
-  element,
-  defaults = {},
-  variantNamespace = `${label}Variants`
-) => ({ ess = {}, as = element, variant, className, ...props }) => {
-  const theme = useTheme()
-  const _className = `ess_${label}` + (className ? ` ${className}` : '')
-  const variantESS = getVariantESS(variantNamespace, variant, theme)
-  const __ess = useESS({ ...variantESS, ...defaults, ...ess })
-  
-  return jsx(as, {
-    css: __ess,
-    className: _className,
-    ...props
-  })
+export default (label, element, defaultProps = {}, variantNamespace = `${label}Variants`) => {
+  const { variant: defaultVariant, className: defaultClassName, ...defaults } = defaultProps
+  return ({ ess = {}, as = element, variant = defaultVariant, className, ...props }) => {
+    const theme = useTheme()
+    const _className =
+      `ess_${label}` +
+      (defaultClassName ? ` ${defaultClassName}` : "") +
+      (className ? ` ${className}` : "")
+    const variantESS = getVariantESS(variantNamespace, variant, theme)
+    const __ess = useESS({ ...variantESS, ...ess })
+
+    return jsx(as, {
+      ...defaults,
+      ...props,
+      css: __ess,
+      className: _className
+    })
+  }
 }
 
 export const useThemeVariants = (key, variant) => {
