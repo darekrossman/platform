a3e6f3a19b859aa2dde6256f221e97e180e7fd8c
Darek Rossman <drossman@64labs.com>
2019-03-05 17:50:35 -0500

    patch

--- packages/ui/src/Image.js
+++ packages/ui/src/Image.js
@@ -1,33 +1,28 @@
 import React from "react"
-import styled from "@64labs/ess"
-
-const ImageContainer = styled(
-  "Image",
-  "div",
-  {
-    ess: {
-      pt: "100%",
-      position: "relative",
-      overflow: "hidden",
-      "& > *": {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        display: "block",
-        maxWidth: "100%"
-      }
-    }
-  },
-  "ImageVariants"
-)
+import Box from "./Box"
 
 const Image = ({ src, ess, imageProps, aspectRatio = 1, ...props }) => {
   return (
-    <ImageContainer ess={{ pt: `${aspectRatio * 100}%`, ...ess }} {...props}>
+    <Box
+      ess={{
+        pt: `-${aspectRatio * 100}%`,
+        position: "relative",
+        overflow: "hidden",
+        "& > *": {
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          display: "block",
+          maxWidth: "100%"
+        },
+        ...ess
+      }}
+      {...props}
+    >
       <img src={src} {...imageProps} />
-    </ImageContainer>
+    </Box>
   )
 }
 
